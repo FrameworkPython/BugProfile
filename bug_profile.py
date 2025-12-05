95fbcee4937f60c373bb8e7e905ad4a898ae52b3
Fᵣₐₘₑwₒᵣₖ ₚyₜₕₒₙ <149888152+FrameworkPython@users.noreply.github.com>
2025-12-05 18:10:40 +0330

    Update bug_profile.py

--- bug_profile.py
+++ bug_profile.py
@@ -1,42 +1,31 @@
-from rubpy import Client
 import asyncio
 import os
+from rubpy import Client
 
 
 async def main():
     try:
-        async with Client(name='rubpy', display_welcome=False) as bot:
-            files = {
-                "profile_img": "img1.jpg",  # عکس پروفایل اصلی
-                "thumb_img": "img2.jpg"     # عکسی که از بیرون میخواین ببینین
-            }
-            
-            for file_name, file_path in files.items():
-                if not os.path.exists(file_path):
-                    raise FileNotFoundError(f"فایل {file_path} یافت نشد.")
-            
-            profile_res = await bot.upload(files["profile_img"])
-            thumb_res = await bot.upload(files["thumb_img"])
-            
-            profile_id = profile_res["file_id"]
-            thumb_id = thumb_res["file_id"]
-            
-            me = await bot.get_me()
-            guid = me["user"]["user_guid"]
-            
-            upload_data = {
-                "object_guid": guid,
-                "thumbnail_file_id": thumb_id,
-                "main_file_id": profile_id,
-            }
-            
-            result = await bot.builder(name='uploadAvatar', input=upload_data)
+        async with Client("rubpy") as bot:
+            profile = "photo.jpg" #عکس اصلی 
+            thumb = "photo1.jpg" # عکس بیرون  
+
+            if not os.path.isfile(profile):
+                raise FileNotFoundError(f"فایل {profile} یافت نشد.")
+            if not os.path.isfile(thumb):
+                raise FileNotFoundError(f"فایل {thumb} یافت نشد.")
+
+            thumb_id = (await bot.upload(thumb)).file_id
+
+            result = await bot.upload_avatar(
+                object_guid="me",
+                image=profile,
+                thumbnail_file_id=thumb_id,
+            )
 
             if result:
                 print("پروفایل و تامبنیل آپلود شدند")
-   
     except Exception as e:
         print(f"خطا: {e}")
 
 
-asyncio.run(main()) 
+asyncio.run(main())
